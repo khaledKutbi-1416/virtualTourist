e3d7fe7b5f01e526b529e38919c895e1945a49d8
khaled <khaledkutbi-1416@hotmail.com>
2020-05-28 03:50:06 +0300

    Initial Commit

---
+++ virtualTourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  virtualTourist
+//
+//  Created by Khaled Kutbi on 05/10/1441 AH.
+//  Copyright © 1441 udacity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
